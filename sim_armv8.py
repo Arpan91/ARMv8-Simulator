ed2e6db06c7e1a3de6fad7640d565e754303e326
Arpan91 <arpanas91@gmail.com>
2014-08-09 04:24:53 +0530

    Add routine for Shift Register addressing mode (32 and 64 bit) added.

--- sim_armv8.py
+++ sim_armv8.py
@@ -16,7 +16,7 @@ class ARMv8(object):
     def add_immediate_32(inst):
 
         """
-        Addition routine for immediate addressing mode(32 bit mode).
+        Addition(ADD) routine for immediate addressing mode(32 bit mode).
         """
 
         rd = int(inst[27:], 2)
@@ -24,17 +24,17 @@ class ARMv8(object):
         res = bin(int(('0'*20 + inst[10:22]), 2) + int(self.gp_registers[rn[-31:]], 2))[2:]
 
         if len(res) > 32:
-            self.gp_registers[rd] = '0'*32 + res[-32:]
+            self.gp_registers[rd] = self.gp_registers[rd][:32]+ res[-32:]
         elif len(res) < 32:
-            self.gp_registers[rd] = '0'*32 + '0'*(32-len(res)) + res
+            self.gp_registers[rd] = self.gp_registers[rd][:32] + '0'*(32-len(res)) + res
         else
-            self.gp_registers[rd] = '0'*32 + res
+            self.gp_registers[rd] = self.gp_registers[rd][:32] + res
 
 
     def add_immediate_64(inst):
 
         """
-        Addition routine for immediate addressing mode(64 bit mode).
+        Addition(ADD) routine for immediate addressing mode(64 bit mode).
         """
 
         rd = int(inst[27:], 2)
@@ -55,19 +55,19 @@ class ARMv8(object):
 
         rd = int(inst[27:], 2)
         rn = int(inst[-10:-5], 2)
-        a, b = int(('0'*20 + inst[10:22]), 2), int(self.gp_registers[rn[-31:]], 2)
+        a, b = int(('0'*20 + inst[10:22]), 2), int(self.gp_registers[rn][-31:], 2)
         res = bin(a+b)[2:]
         
         if int(res, 2) == 0:
             self.flags[Z] = True
         if len(res) > 32:
             self.flags[V] = True
-            self.gp_registers[rd] = '0'*32 + res[-32:]
+            self.gp_registers[rd] = self.gp_registers[rd][:32] + res[-32:]
         elif len(res) < 32:
-            self.gp_registers[rd] = '0'*32 + '0'*(32-len(res)) + res
+            self.gp_registers[rd] = self.gp_registers[rd][:32] + '0'*(32-len(res)) + res
         else
-            self.gp_registers[rd] = '0'*32 + res
-        if self.gp_registers[rd[0]] == '1':
+            self.gp_registers[rd] = self.gp_registers[rd][:32] + res
+        if self.gp_registers[rd][0] == '1':
             self.flags[N] = True
 
 
@@ -89,5 +89,64 @@ class ARMv8(object):
             self.gp_registers[rd] = '0'*(64-len(res)) + res
         else
             self.gp_registers[rd] = res
-        if self.gp_registers[rd[0]] == '1':
+        if self.gp_registers[rd][0] == '1':
             self.flags[N] = True
+
+    def add_shift_register_32(inst):
+
+        """
+        Addition(ADD) routine for shift register addressing mode(32 bit mode).
+        """
+
+        rd = int(inst[27:], 2)
+        rn = int(inst[-10:-5], 2)
+        amount = int(inst[-16:-10], 2)
+        rm = int(inst[-21:-16], 2)
+        shift = inst[-24:-22]
+
+        if shift == '00':
+            rm_new = ('0'*32) + self.gp_registers[rm][(32+amount):] + ('0'*amount)
+        elif shift == '01':
+            rm_new = ('0'*32) + ('0'*amount) + self.gp_registers[rm][32:-amount]
+        elif shift == '10':
+            rm_new = ('0'*32) + (self.gp_registers[rm][32]*amount + self.gp_registers[rm][32:-amount]
+
+        rm = int(rm_new, 2)
+        res = bin(rm + int(self.gp_registers[rn][-32:], 2))[2:]
+        
+        if len(res) > 32:
+            self.gp_registers[rd] = self.gp_registers[rd][:32]+ res[-32:]
+        elif len(res) < 32:
+            self.gp_registers[rd] = self.gp_registers[rd][:32] + '0'*(32-len(res)) + res
+        else
+            self.gp_registers[rd] = self.gp_registers[rd][:32] + res
+
+    def add_shift_register_64(inst):
+
+        """
+        Addition(ADD) routine for shift register addressing mode(64 bit mode).
+        """
+
+        rd = int(inst[27:], 2)
+        rn = int(inst[-10:-5], 2)
+        amount = int(inst[-16:-10], 2)
+        rm = int(inst[-21:-16], 2)
+        shift = inst[-24:-22]
+
+        if shift == '00':
+            rm_new = self.gp_registers[rm][amount:] + ('0'*amount)
+        elif shift == '01':
+            rm_new = ('0'*amount) + self.gp_registers[rm][:-amount]
+        elif shift == '10':
+            rm_new = (self.gp_registers[rm][0]*amount + self.gp_registers[rm][:-amount]
+
+        rm = int(rm_new, 2)
+        res = bin(rm + int(self.gp_registers[rn], 2))[2:]
+        
+        if len(res) > 64:
+            self.gp_registers[rd] = res[-64:]
+        elif len(res) < 64:
+            self.gp_registers[rd] = '0'*(64-len(res)) + res
+        else
+            self.gp_registers[rd] = res
+
